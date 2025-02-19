9a00d0c65b57715f91ad8935a6dc840dd36a50a1
phoelapyae <zinmyomaung17@gmail.com>
2025-02-20 01:51:23 +0700

    added validator and show messages

--- src/components/Form/ShortenerForm.tsx
+++ src/components/Form/ShortenerForm.tsx
@@ -3,13 +3,16 @@ import { GenerateUrl } from "../../network/api";
 
 export default function ShortenerForm()
 {
-    const [error, setError] = useState<string | null>();
-    const [url, setUrl] = useState({
+    const [successMessage, setSuccessMessage] = useState<string | null>(null);
+    const [error, setError]                   = useState<string | null>(null);
+    const [url, setUrl]                       = useState({
         long_url: ''
     });
 
     const handleChange = (event: {target: {name: string, value: string}}) => {
-        setUrl({...url, [event.target.name]: event.target.value});
+        setUrl({ ...url, [event.target.name]: event.target.value });
+        setSuccessMessage(null);
+        setError(null);
     }
 
     const handleSubmit = async(event: React.FormEvent<HTMLFormElement>) => {
@@ -17,11 +20,12 @@ export default function ShortenerForm()
 
         try {
             await GenerateUrl(url);
+            setSuccessMessage('Url generated successfully.');
             setUrl({long_url: ''});
         }
         catch (error)
         {
-            setError('Something went wrong in fetching data.')
+            setError('Please enter a valid url link.')
         }
     }
 
@@ -29,19 +33,27 @@ export default function ShortenerForm()
         <>
             <form
                 onSubmit={handleSubmit}
-                id="link-form" className="relative flex flex-col w-full p-10 space-y-4 bg-neutral-200 rounded-lg md:flex-row md:space-y-0 md:space-x-3"
+                id="link-form" className="relative w-full p-10 space-y-4 bg-neutral-200 rounded-lg"
             >
-                <input
-                    type="text"
-                    name="long_url"
-                    className="flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus-outline-none"
-                    placeholder="Shorten a link here."
-                    value={url.long_url}
-                    onChange={handleChange}
-                />
-                <button className="px-10 py-3 bg-blue-300 rounded-lg hover:bg-cyanLight focus:outline-none md:py-2">
-                    Shorten It!
-                </button>
+                <div className="flex flex-col md:flex-row md:space-y-0 md:space-x-3">
+                    <input
+                        type="text"
+                        name="long_url"
+                        className="flex-1 p-3 border-2 rounded-lg placeholder-gray-400 focus-outline-none"
+                        placeholder="Shorten a link here."
+                        value={url.long_url}
+                        onChange={handleChange}
+                    />
+                    <button type="submit" className="px-10 py-3 bg-blue-300 rounded-lg hover:bg-cyanLight focus:outline-none md:py-2">
+                        Shorten It!
+                    </button>
+                </div>
+                {successMessage && (
+                    <div className="text-green-400 text-md">{successMessage}</div>
+                )}
+                {error && (
+                    <div className="text-red-400 text-md">{error}</div>
+                )}
             </form>
         </>
     );
